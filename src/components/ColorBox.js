a96061d2fb14a94129c39a98665a437f2a8b5342
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-06-19 14:52:17 -0500

    Style ColorBox

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -3,12 +3,16 @@ import './styles/ColorBox.css';
 
 class ColorBox extends React.Component {
   render() {
+    const { background, name } = this.props;
     return (
-      <div
-        style={{ backgroundColor: this.props.background }}
-        className='ColorBox'
-      >
-        <span />
+      <div style={{ background, name }} className='ColorBox'>
+        <div className='copy-container'>
+          <div className='box-content'>
+            <span>{name}</span>
+          </div>
+          <button className='copy-button' />
+        </div>
+        <span className='see-more'>More</span>
       </div>
     );
   }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -4,8 +4,8 @@ import './styles/Palette.css';
 
 class Palette extends React.Component {
   render() {
-    const colorBoxes = this.props.colors.map(color => {
-      return <ColorBox key={color.color} background={color.color} />;
+    const colorBoxes = this.props.colors.map(({ color, name, id }) => {
+      return <ColorBox key={color} background={color} name={name} />;
     });
     return (
       <div className='Palette'>
