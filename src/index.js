ae2f83c95c246fba6cffbb6c58b3cd3eb29b3230
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-03 13:48:31 -0500

    Adding react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' />
+      <Route exact path='/palette/:id' />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
