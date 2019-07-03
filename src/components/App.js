1e516b2bd429fa5225ea9ffac2e4b32a2fdf1316
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-03 14:00:22 -0500

    Finding the Palette

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from '../seedColors';
 import { generatePalette } from '../colorHelpers';
 
 class App extends React.Component {
+  findPalette = id => {
+    return seedColors.find(palette => {
+      return palette.id === id;
+    });
+  };
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Here</h1>} />
+          <Route
+            exact
+            path='/palette/:id'
+            render={routeProps => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id),
+                )}
+              />
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' />
-      <Route exact path='/palette/:id' />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
